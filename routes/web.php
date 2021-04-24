0b185b73a19b4dbd70fb3417015811bcf6a5cb4e
makhosi6 <ndondo330@gmail.com>
2021-04-24 18:49:07 +0200

    Saturday 04/24/2021 18:49 - host config

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,3 +27,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+
+Route::get('/user', function (Request $request) {
+
+    return $request->user();
+
+});
